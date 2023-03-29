15fec80f17241d17b08d9aa35fa5d9bf915f0557
aditya8853 <adityapatil2100@gmail.com>
2023-03-29 18:14:20 +0530

    Sample PageObject added and required dependencies added in pom.xml

--- AjioProject/src/test/java/pageObjects/LoginPage.java
+++ AjioProject/src/test/java/pageObjects/LoginPage.java
@@ -1,5 +1,23 @@
 package pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
 
+	WebDriver driver;
+	public LoginPage()
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="")
+	private WebElement login;
+	public WebElement returnLogin()
+	{
+		return login;
+	}
 }
