a363340199e81804ceb8069c749b7a59b071f598
aditya8853 <adityapatil2100@gmail.com>
2023-03-31 20:18:05 +0530

    Added ChromeOptions to Test runner and Page Factory init elements to POM class.

--- AjioProject/src/test/java/pageObjects/LoginPage.java
+++ AjioProject/src/test/java/pageObjects/LoginPage.java
@@ -3,6 +3,7 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 	public WebDriver driver;
@@ -10,6 +11,7 @@ public class LoginPage {
 	public LoginPage(WebDriver driver) 
 	{
 		this.driver=driver;
+		PageFactory.initElements(driver, this);
 	}
 	
 	@FindBy(xpath="//a[@title='MEN']")
