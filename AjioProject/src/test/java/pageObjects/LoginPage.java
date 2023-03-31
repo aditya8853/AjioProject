54d88d931f38c4f83831c74c627606e12ff975c5
Yogesh <Yogesh@LAPTOP-43QLNKQE>
2023-03-31 16:18:31 +0530

    Formatted Pom Class

--- AjioProject/src/test/java/pageObjects/LoginPage.java
+++ AjioProject/src/test/java/pageObjects/LoginPage.java
@@ -1,7 +1,8 @@
 package pageObjects;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage {
 	public WebDriver driver;
@@ -11,15 +12,61 @@ public class LoginPage {
 		this.driver=driver;
 	}
 	
-	private By men=By.xpath("//a[@title='MEN']");
-	private By Womenen=By.cssSelector("a[title='WOMEN']");
-	private By kids=By.cssSelector("a[title='KIDS']");
-	private By Indie=By.cssSelector("a[title='INDIE']");
-	private By Home_And_Kitchen=By.cssSelector("a[title='HOME AND KITCHEN']");
-	private By Search=By.cssSelector("input[placeholder='Search AJIO']");
-	private By Bag=By.cssSelector(".ic-cart");
-	private By SignInBtn=By.xpath("(//a[@href='/selfcare'])[1]");
+	@FindBy(xpath="//a[@title='MEN']")
+	private WebElement mens; 
+	public WebElement menSection()
+	{
+		return mens;
+	}
 	
+	@FindBy(css="a[title='WOMEN']")
+	private WebElement Womens;
+	public WebElement WomensSection()
+	{
+		return Womens;
+	}
+	
+	@FindBy(css="a[title='KIDS']")
+	private WebElement Kids;
+	public WebElement kids()
+	{
+		return Kids;
+	}
+	
+	@FindBy(css="a[title='INDIE']")
+	private WebElement Indie;
+	public WebElement Indie()
+	{
+		return Indie;
+	}
+	
+	@FindBy(css="a[title='HOME AND KITCHEN']")
+	private WebElement Home_And_Kitchen;
+	public WebElement Home_And_Kitchen()
+	{
+		return Home_And_Kitchen;
+	}
+	
+	@FindBy(css="input[placeholder='Search AJIO']")
+	private WebElement Search;
+	public WebElement Search()
+	{
+		return Search;
+	}
+	
+	@FindBy(css=".ic-cart")
+	private WebElement Bag;
+	public WebElement Bag()
+	{
+		return Bag;
+	}
+	
+	@FindBy(xpath="(//a[@href='/selfcare'])[1]")
+	private WebElement SignInBtn;
+	public WebElement SignInBtn()
+	{
+		return SignInBtn;
+	}
 	
 	
 }
