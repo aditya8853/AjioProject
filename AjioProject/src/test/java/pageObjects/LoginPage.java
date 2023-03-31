16a2d23291bc73dc8898f6cf1b7287d1705e823c
Yogesh <Yogesh@LAPTOP-43QLNKQE>
2023-03-31 11:34:07 +0530

    Added login page object

--- AjioProject/src/test/java/pageObjects/LoginPage.java
+++ AjioProject/src/test/java/pageObjects/LoginPage.java
@@ -1,23 +1,25 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-
-	WebDriver driver;
-	public LoginPage()
+	public WebDriver driver;
+	
+	LoginPage(WebDriver driver) 
 	{
 		this.driver=driver;
-		PageFactory.initElements(driver, this);
 	}
 	
-	@FindBy(xpath="")
-	private WebElement login;
-	public WebElement returnLogin()
-	{
-		return login;
-	}
+	private By men=By.xpath("//a[@title='MEN']");
+	private By Womenen=By.cssSelector("a[title='WOMEN']");
+	private By kids=By.cssSelector("a[title='KIDS']");
+	private By Indie=By.cssSelector("a[title='INDIE']");
+	private By Home_And_Kitchen=By.cssSelector("a[title='HOME AND KITCHEN']");
+	private By Search=By.cssSelector("input[placeholder='Search AJIO']");
+	private By Bag=By.cssSelector(".ic-cart");
+	private By SignInBtn=By.xpath("(//a[@href='/selfcare'])[1]");
+	
+	
+	
 }
